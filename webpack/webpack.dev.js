5531f705a8a9d24d10cad1b90b1a2f1ffb920bef
Paulo Rezende <rezendepauloedu@outlook.com>
2018-06-17 18:14:04 -0300

    Add stats option and optimizes paths on devServer

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,5 +1,6 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const merge = require('webpack-merge');
+const path = require('path');
 
 const common = require('./webpack.common');
 
@@ -15,10 +16,11 @@ module.exports = merge(common, {
     ]
   },
   devServer: {
-    contentBase: './build',
-    publicPath: '/public/',
+    contentBase: path.resolve('build'),
+    publicPath: '/public',
     open: true,
-    port: 8080
+    port: 8080,
+    stats: 'minimal'
   },
   plugins: [
     new MiniCssExtractPlugin({
