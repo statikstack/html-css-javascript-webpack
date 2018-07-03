c7b922686a4eebb0efe0a614b8e90d3b8364333a
Paulo Rezende <rezendepauloedu@outlook.com>
2018-07-02 21:40:48 -0300

    Add support to cache files on Webpack configs

--- webpack.common.js
+++ webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
     vendor: './statik/scripts/vendor.js'
   },
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].[chunkhash].bundle.js',
     path: path.resolve('dist')
   },
   module: {
--- webpack.prod.js
+++ webpack.prod.js
@@ -55,9 +55,17 @@ module.exports = merge(common, {
     ]
   },
   optimization: {
+    runtimeChunk: 'single',
     splitChunks: {
       chunks: 'all',
-      name: true
+      name: true,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
     },
     minimizer: [
       new OptimizeCSSAssetsPlugin({
@@ -73,6 +81,7 @@ module.exports = merge(common, {
     new UglifyJsWebpackPlugin({
       sourceMap: true
     }),
+    new webpack.HashedModuleIdsPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     })
